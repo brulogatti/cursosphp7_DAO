bcd117fdaebb228ee5e8a69e5cd6393d10068d0d
brulogatti <bru.logatti@gmail.com>
2023-02-21 12:56:55 -0300

    Inicio do projeto

---
+++ class/Sql.php
@@ -0,0 +1,48 @@
+<?php
+class Sql extends PDO{
+
+    private $conn;
+
+    public function __construct()
+    {
+        $this->conn = new PDO("mysql:host=localhost; dbname=dbphp7", "root", "");
+    }
+
+    private function setParams($statement, $parameters=array()){
+        foreach ($parameters as $key => $value) {
+            $this->setParam($statement,$key,$value);
+        }
+    }
+
+    private function setParam($statement, $key, $value){
+        $statement->bindParam($key,$value);
+    }
+
+    public function queryAll($rawQuery, $params = array())
+    {
+        $stmt = $this->conn->prepare($rawQuery);
+
+        $this->setParams($stmt,$params);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    /*public function runQuery($rawQuery, $params = array())
+    {
+        $stmt = $this->conn->prepare($rawQuery);
+
+        $this->setParams($stmt,$params);
+
+        return $stmt->execute();
+    }*/
+
+    public function select($rawQuery, $params=array()):array{
+        $stmt = $this->queryAll($rawQuery,$params);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}
+?>
