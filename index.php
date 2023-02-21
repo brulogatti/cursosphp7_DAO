9b5197263b9be16c39b03d490eb96c949db8c8e5
brulogatti <bru.logatti@gmail.com>
2023-02-21 13:48:08 -0300

    Criação funcao login listar

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php
 require_once("config.php");
 
+/*
 $root = new Usuario();
-
 $root->loadById(3);
 
 echo $root;
+*/
+
+//$lista = Usuario::getList();
+
+//$busca = Usuario::search("jo");
+
+$usuario = new Usuario();
+
+$usuario->login("root", "!@#$%");
+
+echo $usuario;
+
+//echo json_encode($lista);
+
+//echo json_encode($busca);
 
 ?>
